7948f7c7da818110d2c24a72af693108578db872
JULIPM02 <jpalacio85@soy.sena.edu.co>
2024-11-05 18:35:26 -0500

    componente Campo Estrellado

--- CosmoViaje/src/App.jsx
+++ CosmoViaje/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import StarField from './components/StarrySpace/StarrySpace';
+// import CustomModal from './components/modal/CustomModal';
+// import CardData from './components/CardData/CardData';
+// import { Button } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <StarField />
+      
+      {/* <Button variant="contained" color="primary" onClick={handleOpen}>
+        Abrir Modal
+      </Button>
+      <CustomModal open={open} handleClose={handleClose} />
+      <h1 style={{zIndex: 1000, position: 'relative'}}></h1>
+      <CardData style={{zIndex: 1000, position: 'relative'}}></CardData> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
