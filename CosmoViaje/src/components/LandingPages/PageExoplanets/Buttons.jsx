2d04df6cd30dcabc1aa873a73be80d0df283863d
Paulina <fpaulina545@gmail.com>
2024-11-05 20:52:26 -0500

    Page Exoplanets

---
+++ CosmoViaje/src/components/LandingPages/PageExoplanets/Buttons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Buttons() {
+    return (
+        <div className="button-group">
+            <button className="btn scientific-data">
+                DATOS CIENTÍFICOS</button>
+            <button className="btn gallery">
+                GALERIA</button>
+        </div>
+    );
+}
+
+export default Buttons;
---
+++ CosmoViaje/src/components/LandingPages/PageExoplanets/Description.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Description() {
+    return (
+        <div>
+            <h1 className="welcome-message">BIENVENIDX AL UNIVERSO EXOPLANETAS</h1>
+            <p className="description"> ¡Disfruta tu recorrido explorando imágenes <br></br> y datos científicos de los Exoplanetas descubiertos en los últimos 5 años! </p>
+        </div>
+    );
+}
+
+export default Description;
